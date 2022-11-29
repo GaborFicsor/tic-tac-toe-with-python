893d89c3febd5647702d91d8ab2bf1e81327feea
GaborFicsor <ficsorgabor@gmail.com>
2022-11-29 21:42:49 +0000

    Add further functionality to winner function that checks for columns, rows or diagonal results

--- run.py
+++ run.py
@@ -33,6 +33,27 @@ class TicTacToe:
             return True
         return False
 
+    def winner(self, square, letter):
+        row_ind = square // 3
+        row = self.board[row_ind*3:(row_ind + 1) * 3]
+        if all([spot == letter for spot in row]):
+            return True
+
+        col_ind = square % 3
+        column = [self.board[col_ind+i*3] for i in range(3)]
+        if all([spot == letter for spot in column]):
+            return True
+
+        if square % 2 == 0:
+            diagonal1 = [self.board[i] for i in [0, 4, 8]]
+            if all([spot == letter for spot in diagonal1]):
+                return True
+            diagonal2 = [self.board[i] for i in [2, 4, 6]]
+            if all([spot == letter for spot in diagonal2]):
+                return True
+
+        return False
+
 
 def play(game, x_player, o_player, print_game=True):
     if print_game:
@@ -51,8 +72,16 @@ def play(game, x_player, o_player, print_game=True):
                 game.print_board()
                 print('')
 
+            if game.current_winner:
+                if print_game:
+                    print(letter + ' wins!')
+                return letter
+
             letter = 'O' if letter == 'X' else 'X'
             if letter == 'X':
                 letter = 'O'
             else:
                 letter = 'X'
+
+        if print_game:
+            print("It's a tie!")
