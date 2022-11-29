1d3bd5479156b9901dd772575b56d6a286d44b7b
GaborFicsor <ficsorgabor@gmail.com>
2022-11-29 21:21:56 +0000

    Fix various syntax errors

--- run.py
+++ run.py
@@ -4,18 +4,18 @@ class TicTacToe:
         self.current_winner = None
 
     def print_board(self):
-        for rown in [self.board[i*3:(i+1)*3] for i in range(3)]:
+        for row in [self.board[i*3:(i+1)*3] for i in range(3)]:
             print("| " + ' | '.join(row) + ' |')
 
     @staticmethod
     def print_board_nums():
         number_board = [[str(i) for i in range(j*3, (j+1)*3)] for j in range(3)]
-        for rown in number_board:
+        for row in number_board:
             print("| " + ' | '.join(row) + ' |')
 
     def available_moves(self):
         moves = []
-        for(i, spot) in enumerate(self.board):
+        for i, spot in enumerate(self.board):
             if spot == ' ':
                 moves.append(i)
 
@@ -42,3 +42,7 @@ def play(game, x_player, o_player, print_game=True):
             square = o_player.get_move(game)
         else:
             square = x_player.get_move(game)
+
+        if game.make_move(square, letter):
+            if print_game:
+                print(letter + f' makes a move to square {square}')
