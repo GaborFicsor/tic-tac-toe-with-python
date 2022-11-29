a042ebc15c30ec5ab7b9f84e9a2a382a3e296c54
GaborFicsor <ficsorgabor@gmail.com>
2022-11-29 20:49:36 +0000

    Add TicTacToe Class to run.py

---
+++ run.py
@@ -0,0 +1,8 @@
+class TicTacToe:
+    def __init__(self):
+        self.board = [' ' for _ in range(9)]
+        self.current_winner = None
+
+    def print-board(self):
+        for rown in [self.board[i*3:(i+1)*3] for i in range(3)]:
+            print("| " + ' | '.join(row) + ' |')
