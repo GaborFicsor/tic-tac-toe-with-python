393e2835499ddbbf881336ba8b744018072a6bd8
GaborFicsor <ficsorgabor@gmail.com>
2022-11-29 20:53:07 +0000

    Add static method that prints the board with the reference indices

--- run.py
+++ run.py
@@ -6,3 +6,9 @@ class TicTacToe:
     def print-board(self):
         for rown in [self.board[i*3:(i+1)*3] for i in range(3)]:
             print("| " + ' | '.join(row) + ' |')
+
+    @staticmethod
+    def print_board_nums():
+        number_board = [[str(i) for i in range(j*3, (j+1)*3)] for j in range(3)]
+        for rown in number_board:
+            print("| " + ' | '.join(row) + ' |')
